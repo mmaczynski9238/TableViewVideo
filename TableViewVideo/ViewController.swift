3d85a70de8d558f96ded72480fb4cf8335385594
Michael Maczynski <mmaczynski@jh-mbair-158-17.d214.org>
2016-02-23 14:34:39 -0600

    done

--- TableViewVideo/ViewController.swift
+++ TableViewVideo/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    var cities = ["Chicago", "San Fransisco", "Dallas", "Paris", "Rome", "New York"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        
     }
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return cities.count
     }
-
+    
+    
+    
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+       // var myCell = tableViewOutlet.dequeueReusableCellWithIdentifier("myCell")! as UITableViewCell
+        var myCell = tableViewOutlet.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as UITableViewCell
+        myCell.textLabel?.text = cities[indexPath.row]
+        return myCell
+        
+    }
 
 }
 
